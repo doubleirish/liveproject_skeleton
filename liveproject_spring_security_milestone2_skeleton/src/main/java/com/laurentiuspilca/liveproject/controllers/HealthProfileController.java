f669040138bd7a30729edc97343bd687ffc813ea
Conor Redmond <credmond@incomm.com>
2020-09-13 21:58:34 -0700

    add auth debug logging on profile controller

--- liveproject_spring_security_milestone2_skeleton/src/main/java/com/laurentiuspilca/liveproject/controllers/HealthProfileController.java
+++ liveproject_spring_security_milestone2_skeleton/src/main/java/com/laurentiuspilca/liveproject/controllers/HealthProfileController.java
@@ -3,8 +3,10 @@ package com.laurentiuspilca.liveproject.controllers;
 import com.laurentiuspilca.liveproject.entities.HealthProfile;
 import com.laurentiuspilca.liveproject.services.HealthProfileService;
 // TODO import org.springframework.security.core.Authentication;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
-
+@Slf4j
 @RestController
 @RequestMapping("/profile")
 public class HealthProfileController {
@@ -16,12 +18,14 @@ public class HealthProfileController {
   }
 
   @PostMapping
-  public void addHealthProfile(@RequestBody HealthProfile healthProfile) {
+  public void addHealthProfile(@RequestBody HealthProfile healthProfile, Authentication authentication) {
+    log.info("profile-user={}, authentication-user={} ",healthProfile.getUsername(), authentication.getPrincipal());
     healthProfileService.addHealthProfile(healthProfile);
   }
 
   @GetMapping("/{username}")
-  public HealthProfile findHealthProfile(@PathVariable String username) {
+  public HealthProfile findHealthProfile(@PathVariable String username, Authentication authentication) {
+    log.info("profile-user={}, authentication-user={} ",username, authentication.getPrincipal());
     return healthProfileService.findHealthProfile(username);
   }
 
